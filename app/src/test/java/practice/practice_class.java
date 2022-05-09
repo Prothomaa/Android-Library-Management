2d896a68a73dab53c7c679d4071535f45a126ec4
barbie <prothomaaminhaz@gmail.com>
2022-05-09 16:54:31 +0600

    added new java file

---
+++ app/src/test/java/practice/practice_class.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class practice_class {
+
+    int roll ;
+    practice_class(int roll){
+        this.roll;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
