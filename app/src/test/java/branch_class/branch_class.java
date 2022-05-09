3f0562bc8a496c60af41d65e13febe96463d03f5
barbie <prothomaaminhaz@gmail.com>
2022-05-09 18:33:48 +0600

    added a file in b1 branch

---
+++ app/src/test/java/branch_class/branch_class.java
@@ -0,0 +1,16 @@
+package branch_class;
+
+public class branch_class {
+    float cgpa ;
+    branch_class(float cgpa){
+        this.cgpa;
+    }
+
+    public float getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+}
