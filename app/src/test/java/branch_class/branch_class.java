bc4427c8ff5009125e10bfd171b526ca4a9b4dbc
barbie <prothomaaminhaz@gmail.com>
2022-05-10 00:19:46 +0600

    change in branch_class java file from master

--- app/src/test/java/branch_class/branch_class.java
+++ app/src/test/java/branch_class/branch_class.java
@@ -2,15 +2,35 @@ package branch_class;
 
 public class branch_class {
     float cgpa ;
+    String session;
+    int id;
     branch_class(float cgpa){
         this.cgpa;
+        this.session;
+        this.id
     }
 
-    public float getCgpa() {
+    private float getCgpa() {
         return cgpa;
     }
 
-    public void setCgpa(float cgpa) {
+    private void setCgpa(float cgpa) {
         this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
     }
 }
