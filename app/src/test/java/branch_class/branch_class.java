521588ed770289f521535d7a75529b449f5fedb2
barbie <prothomaaminhaz@gmail.com>
2022-05-09 23:59:59 +0600

    change from b3 in branch_class

--- app/src/test/java/branch_class/branch_class.java
+++ app/src/test/java/branch_class/branch_class.java
@@ -2,12 +2,22 @@ package branch_class;
 
 public class branch_class {
     float cgpa ;
+    char name ;
     branch_class(float cgpa){
         this.cgpa;
+        this.name
     }
 
     public float getCgpa() {
         return cgpa;
+    }
+
+    public char getName() {
+        return name;
+    }
+
+    public void setName(char name) {
+        this.name = name;
     }
 
     public void setCgpa(float cgpa) {
